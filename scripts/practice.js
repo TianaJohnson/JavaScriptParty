d6ec96195ff80d3d5f9a4607ec7c88f7bbf82855
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-28 19:34:22 -0600

    first try didnt work, syntax has changed

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,11 +1,18 @@
-const divide = (num) => {
-    if(num % 3 == 0 && num % 5 == 0){
-    return 'Fizzbuzz';
-}else if(num % 3 == 0){
-    return 'Boop';
-}else if(num % 5 == 0){
-    return 'Buzz';
-}else{
-    return num;
-}};
-module.exports = divide;
+const practice = {
+    add:(num1, num2) => num1 + num2 
+}
+
+module.export = practice
+
+
+// const divide = (num) => {
+//     if(num % 3 == 0 && num % 5 == 0){
+//     return 'Fizzbuzz';
+// }else if(num % 3 == 0){
+//     return 'Boop';
+// }else if(num % 5 == 0){
+//     return 'Buzz';
+// }else{
+//     return num;
+// }};
+// module.exports = divide;
