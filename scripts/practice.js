f369d1e3cd618443c743ffbe7efb62f9d8cb43ae
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-28 19:43:46 -0600

    trouble shooting testing

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,18 +1,18 @@
-const practice = {
-    add:(num1, num2) => num1 + num2 
-}
+// const practice = {
+//     add:(num1, num2) => num1 + num2 
+// }
 
-module.export = practice
+// module.export = practice
 
 
-// const divide = (num) => {
-//     if(num % 3 == 0 && num % 5 == 0){
-//     return 'Fizzbuzz';
-// }else if(num % 3 == 0){
-//     return 'Boop';
-// }else if(num % 5 == 0){
-//     return 'Buzz';
-// }else{
-//     return num;
-// }};
-// module.exports = divide;
+const divide = (num) => {
+    if(num % 3 == 0 && num % 5 == 0){
+    return 'Fizzbuzz';
+}else if(num % 3 == 0){
+    return 'Boop';
+}else if(num % 5 == 0){
+    return 'Buzz';
+}else{
+    return num;
+}};
+module.exports = divide;
--- scripts/practice.test.js
+++ scripts/practice.test.js
@@ -1,28 +1,28 @@
 //const practice = require ('./practice');
 
-test ('add 2 + 2 to equal 4',() =>{
-    expect( 2+2 ).toBe(4);
-});
+// test ('add 2 + 2 to equal 4',() =>{
+//     expect( 2+2 ).toBe(4);
+// });
 
 //didnt work
 // test('Adds 2 + 2 to equal 4', () => {
 //     expect(practice.add( 2, 2 )).toBe( 4 );
 // });
 
-// const divide = require('./practice');
+const divide = require('./practice');
 
-// test('If the number is divisible by 3, return "Boop"',() => {
-//     expect(divide(3)).toBe('Boop');
-//     });
+test('If the number is divisible by 3, return "Boop"',() => {
+    expect(divide(3)).toBe('Boop');
+    });
 
-// test('If the number is divisible by 5, return "Buzz"',() => {
-//     expect(divide(5)).toBe('Buzz');
-// });
+test('If the number is divisible by 5, return "Buzz"',() => {
+    expect(divide(5)).toBe('Buzz');
+});
         
-// test('If the number is divisible by both 3 and 5, return "FizzBuzz"',() => {
-//     expect(divide(15)).toBe('Fizzbuzz');
-// })
+test('If the number is divisible by both 3 and 5, return "FizzBuzz"',() => {
+    expect(divide(15)).toBe('Fizzbuzz');
+})
 
-// test('If the number is not divisable by 3 or 5 than return num', () => {
-//     expect(divide(1)).toBe(1);
-// })
+test('If the number is not divisable by 3 or 5 than return num', () => {
+    expect(divide(1)).toBe(1);
+})
