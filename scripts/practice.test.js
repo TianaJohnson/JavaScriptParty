926c05c9270e7cd2d2cafe6c655901f11ca46b31
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-28 19:42:02 -0600

    testing is off, pages are not actually linking up

--- scripts/practice.test.js
+++ scripts/practice.test.js
@@ -1,7 +1,7 @@
-const practice = require ('./practice');
+//const practice = require ('./practice');
 
 test ('add 2 + 2 to equal 4',() =>{
-    expect(2+2).toBe(4);
+    expect( 2+2 ).toBe(4);
 });
 
 //didnt work
