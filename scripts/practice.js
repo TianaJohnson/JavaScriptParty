1b1c05aa608cf21797f946d83e79fc7b0f48b4c7
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-28 19:51:55 -0600

    hmm

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,18 +1,21 @@
 // const practice = {
 //     add:(num1, num2) => num1 + num2 
 // }
+const practice = (num1 , num2) => {
+    return num1 + num2;
+}
 
-// module.export = practice
+module.export = practice;
 
 
-const divide = (num) => {
-    if(num % 3 == 0 && num % 5 == 0){
-    return 'Fizzbuzz';
-}else if(num % 3 == 0){
-    return 'Boop';
-}else if(num % 5 == 0){
-    return 'Buzz';
-}else{
-    return num;
-}};
-module.exports = divide;
+// const divide = (num) => {
+//     if(num % 3 == 0 && num % 5 == 0){
+//     return 'Fizzbuzz';
+// }else if(num % 3 == 0){
+//     return 'Boop';
+// }else if(num % 5 == 0){
+//     return 'Buzz';
+// }else{
+//     return num;
+// }};
+// module.exports = divide;
