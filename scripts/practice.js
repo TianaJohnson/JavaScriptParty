5bb26299b66fb6216cbd9f38340279e5b18b2b22
Tiana Johnson <tiana.marie013@gmail.com>
2019-11-30 11:00:29 -0600

    axious fetch from JSONplaceholder RESTapi

--- scripts/practice.js
+++ scripts/practice.js
@@ -7,9 +7,13 @@ const practice = {
         user ['lastName'] = 'Johnson';
         return user;
     }
+    fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
+
 };
 
-fetchUser: () +> axios.get
+
 // const practice = (num1 , num2) => {
 //     return num1 + num2;
 // }
