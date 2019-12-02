52bb363449b2934ac266e47903ed400882bf86b8
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-02 11:44:43 -0600

    accidently deleted some stuff, got it back

--- scripts/practice.js
+++ scripts/practice.js
@@ -1,15 +1,20 @@
+const axios = require('axios');
+
 const practice = {
-    add:(num1, num2) => num1 + num2,
     isNull: () => null,
     checkValue: (p) => p,
     createUser: () => {
+        const  
         const user = {firstName: 'Tiana'}
-        user ['lastName'] = 'Johnson';
+        user ['firstName'] = 'Johnson';
         return user;
-    },
-    fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
-    .then(res => res.data)
-    .catch(err => 'error')
+    }
+};
+    fetchUser: () =>
+     axios
+        .get('https://jsonplaceholder.typicode.com/users/1')
+        .then(res => res.data)
+        .catch(err => 'error')
 
 };
 
--- scripts/practice.test.js
+++ scripts/practice.test.js
@@ -50,11 +50,10 @@ test('Sam should be in usernames', ()=> {
 //expect.assertions is used with async code 
 test('User fetched name should be Leanne Graham', ()=> {
     expect.assertions(1);
-    return functions.fetchUser()
-        .then(data => {
+    return functions.fetchUser().then(data => {
             expect(data.name).toEqual('Leanne Graham')
-        })
-})
+        });
+});
 
 
 
