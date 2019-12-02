1ccb733aedfebeff76c0eda068e0b5894303d48b
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-01 19:02:11 -0600

    cleaning up

--- scripts/practice.js
+++ scripts/practice.js
@@ -13,22 +13,5 @@ const practice = {
 
 };
 
-
-// const practice = (num1 , num2) => {
-//     return num1 + num2;
-// }
-
 module.exports = practice;
 
-
-// const divide = (num) => {
-//     if(num % 3 == 0 && num % 5 == 0){
-//     return 'Fizzbuzz';
-// }else if(num % 3 == 0){
-//     return 'Boop';
-// }else if(num % 5 == 0){
-//     return 'Buzz';
-// }else{
-//     return num;
-// }};
-// module.exports = divide;
