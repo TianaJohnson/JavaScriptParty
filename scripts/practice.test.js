15a680bdaf79139b28bd5ec95cc16d6d5bb64249
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-01 19:02:57 -0600

    ...

--- scripts/practice.test.js
+++ scripts/practice.test.js
@@ -65,24 +65,7 @@ test('User fetched name should be Leanne Graham', ()=> {
 // toBeDefined is the opposite of toBeUndefined
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsy matches anything that an if statement trats as false 
-// toEqual 
 
 
 
-// const divide = require('./practice');
 
-// test('If the number is divisible by 3, return "Boop"',() => {
-//     expect(divide(3)).toBe('Boop');
-//     });
-
-// test('If the number is divisible by 5, return "Buzz"',() => {
-//     expect(divide(5)).toBe('Buzz');
-// });
-
-// test('If the number is divisible by both 3 and 5, return "FizzBuzz"',() => {
-//     expect(divide(15)).toBe('Fizzbuzz');
-// })
-
-// test('If the number is not divisable by 3 or 5 than return num', () => {
-//     expect(divide(1)).toBe(1);
-// })
