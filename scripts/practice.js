9d80e06a4e2f3b05ca9457f6f556aaa9686bf83f
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-01 19:24:41 -0600

    loop file

--- scripts/practice.js
+++ scripts/practice.js
@@ -13,20 +13,5 @@ const practice = {
 
 };
 
-let numbers = [2, 4, 9, 2, 0, 16, 24]
-var largest = numbers[0];
-var smallest = numbers[0];
-for (var i = 1; i < numbers.length; i++) {
-    if (numbers[i] > largest) {
-        largest = numbers[i];
-    }
-    else if(numbers[i] < smallest) {
-        smallest = numbers[i];
-    }
-}
-    console.log(largest);
-    console.log(smallest);
-
-
 module.exports = practice;
 
