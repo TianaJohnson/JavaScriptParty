47ae432ab75633844991354a8eaca38c803b22a4
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-03 22:09:52 -0600

    in with the old test

--- scripts/practice.js
+++ scripts/practice.js
@@ -9,13 +9,25 @@ const practice = {
         user ['lastName'] = 'Johnson';
         return user;
     },
-    fetchUser: () =>
+    fetchUser: () =>{
      axios
         .get('https://jsonplaceholder.typicode.com/users/1')
         .then(res => res.data)
         .catch(err => 'error')
-
+    },
+    divide: (num) => { 
+            if(num % 3 == 0 && num % 5 == 0){
+            return 'Fizzbuzz';
+        }else if(num % 3 == 0){
+            return 'Fizz';
+        }else if(num % 5 == 0){
+            return 'Buzz';
+        }else{
+            return num;
+        }}
 };
+
+
 
 module.exports = practice;
 
