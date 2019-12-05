d2f3851dbff7c80f9dda2067684a5338e0ac9dc9
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-05 13:06:40 -0600

    descriptions of methods for future refrence

--- scripts/reverseString.js
+++ scripts/reverseString.js
@@ -1,8 +1,8 @@
 const reverseString = str =>
 str
     .toLowerCase() // if uppercase is present, turns all into lower case, removes case sensitivity
-    .split('')
-    .reverse()
-    .join('');
+    .split('')  //method is used to split a string into an array of substrings, and returns the new array
+    .reverse()  //method reverses the order of the elements in an array.
+    .join('');  //method returns the array as a string.
 
 module.exports = reverseString;
